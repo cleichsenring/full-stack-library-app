e84216c3884cc2e824341e1f470a3766709784e4
Chris Leichsenring <5677805+cleichsenring@users.noreply.github.com>
2020-10-11 13:07:34 -0400

    initial client commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Project 10. Finish line!</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
