68779ec40128c20f6b4af52760ca668ff21954fe
Chris Leichsenring <5677805+cleichsenring@users.noreply.github.com>
2020-10-12 16:01:08 -0400

    courses and course components

---
+++ client/src/components/Course.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Course = (course) => {
+  return (
+    <div className="grid-33">
+      <Link className="course--module course--link" to={`/courses/${course.id}`} > 
+        <h4 className="course--label">Course</h4>
+        <h3 className="course--title">{course.title}</h3>
+      </Link>
+    </div>
+  )
+}
+
+
+export default Course;
