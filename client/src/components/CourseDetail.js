b26a31ffe5c2f7c8f5e9d3ec7447b6072eed25d4
Chris Leichsenring <5677805+cleichsenring@users.noreply.github.com>
2020-10-17 21:16:50 -0400

    course detail component

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import { APIHelper } from '../APIHelper';
+const helper = new APIHelper();
 
-export default class extends Component {
+
+export default class CourseDetail extends Component {
+   state = { course: null }
+
+  componentDidMount() {
+    helper.getCourse(this.props.match.params.id).then(res => this.setState({course: res}))
+  }
+
+  deleteCourse() {
+    //TODO fix this
+    helper.deleteCourse()
+      .then(this.props.history.push('/'))
+      .catch(this.props.history.push('/error'))
+  }
+
   render() {
+    if(!this.state.course) {
+      return ("Loading course....")
+    }
+    const { course } = this.state;
+    const { id } = this.props.match.params;
+    
     return (
       <div>
         <div className="actions--bar">
           <div className="bounds">
             <div className="grid-100">
               <span>
-                <Link className="button" to="#">Update Course</Link>
-                <Link className="button" >Delete Course</Link>
+                <Link className="button" to={`/courses/${id}/update`}>Update Course</Link>
+                {/* TODO Add delete method */}
+                <button className="button" onClick={this.deleteCourse} >Delete Course</button>
               </span>
                 <Link className="button button-secondary" to="/">Return to List</Link>
             </div>
@@ -21,11 +44,11 @@ export default class extends Component {
           <div className="grid-66">
             <div className="course--header">
               <h4 className="course--label">Course</h4>
-              <h3 className="course--title">Course title here</h3>
-              <p>By AUTHOR</p>
+              <h3 className="course--title">{course.title}</h3>
+              <p>By {course.User.firstName} {course.User.lastName}</p>
             </div>
             <div className="course--description">
-              <p>This will be the course description</p>
+              <p>{course.description}</p>
             </div>
           </div>
           <div className="grid-25 grid-right">
@@ -33,15 +56,11 @@ export default class extends Component {
               <ul className="course--stats--list">
                 <li className="course--stats--list--item">
                   <h4>Estimated Time</h4>
-                  <h3>TIME ESTIMATE</h3>
+                  <h3>{course.estimatedTime}</h3>
                 </li>
                 <li className="course--stats--list--item">
                   <h4>Materials Needed</h4>
-                  <ul>
-                    <li>Item 1</li>
-                    <li>Item 2</li>
-                    <li>Item 3</li>
-                  </ul>
+                    <p>{course.materialsNeeded}</p>
                 </li>
               </ul>
             </div>
