b2e4349a442845dcf69a13ebc439d4ba1cb0bf43
Chris Leichsenring <5677805+cleichsenring@users.noreply.github.com>
2020-10-17 21:16:03 -0400

    signup component

---
+++ client/src/components/UserSignUp.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Form from './Form';
+
+import { APIHelper } from '../APIHelper';
+const helper = new APIHelper();
+
+export default class UserSignUp extends Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    emailAddress: '',
+    password: '',
+    confirmPassword: '',
+    errors: [],
+  }
+
+  render() {
+    const {
+      firstName,
+      lastName,
+      emailAddress,
+      password,
+      confirmPassword,
+      errors,
+    } = this.state;
+
+    return (
+      <div className="bounds">
+        <div className="grid-33 centered signin">
+          <h1>Sign Up</h1>
+          <Form 
+            cancel={this.cancel}
+            errors={errors}
+            submit={this.submit}
+            submitButtonText="Sign Up"
+            elements={() => (
+              <React.Fragment>
+                <input 
+                  id="firstName"
+                  name="firstName"
+                  type="text"
+                  value={firstName}
+                  onChange={this.change}
+                  placeholder="First Name" />
+                <input 
+                  id="lastName"
+                  name="lastName"
+                  type="text"
+                  value={lastName}
+                  onChange={this.change}
+                  placeholder="Last Name" />
+                <input 
+                  id="emailAddress"
+                  name="emailAddress"
+                  type="text"
+                  value={emailAddress}
+                  onChange={this.change}
+                  placeholder="Email Address" />
+                <input 
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={password}
+                  onChange={this.change}
+                  placeholder="Password" />
+                <input 
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  type="password"
+                  value={confirmPassword}
+                  onChange={this.change}
+                  placeholder="Confirm Password" />
+              </React.Fragment>
+            )}
+          />
+          <p>Already have a user account? <Link to="/signin">Click here</Link> to sign in!</p>
+        </div>
+      </div>      
+      
+    );
+  }
+
+  change = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    this.setState(() => {
+      return { [name]: value };
+    });
+  }
+
+  submit = () => {
+
+  }
+
+  cancel = () => {
+    this.props.history.push('/');
+  }
+}
