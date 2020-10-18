2a0d6bbb91d9f0dccece83f5f53c22e08773422b
Chris Leichsenring <5677805+cleichsenring@users.noreply.github.com>
2020-10-17 21:20:09 -0400

    components and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+// Component Imports
+import Header from './components/Header';
+import Courses from './components/Courses';
+import CourseDetail from './components/CourseDetail';
+import CreateCourse from './components/CreateCourse';
+import UpdateCourse from './components/UpdateCourse';
+import UserSignUp from './components/UserSignUp';
+import UserSignIn from './components/UserSignIn';
+import UserSignOut from './components/UserSignOut';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Project 10. Finish line!</h2>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Courses} />
+          <Route path="/courses/create" component={CreateCourse} />
+          <Route path="/courses/:id" component={CourseDetail} />
+          <Route path="/courses/:id/update" component={UpdateCourse} />
+          <Route path="/signin" component={UserSignIn} />
+          <Route path="/signup" component={UserSignUp} />
+          <Route path="/signout" component={UserSignOut} />
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
