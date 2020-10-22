bd0bd41265f06a9c5d9cd38060adeace30f4f361
Chris Leichsenring <5677805+cleichsenring@users.noreply.github.com>
2020-10-22 19:11:11 -0400

    500 error

--- client/src/components/UnhandledError.js
+++ client/src/components/UnhandledError.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const UnhandledError = () => {
   return (
-    <h1>500 error page</h1>
+    <div className="bounds">
+      <h1>Error</h1>
+      <p>An unexpected error has occurred.</p>
+      <NavLink to={'/'} className="button button-secondary">Return to Courses</NavLink>
+
+    </div>
   )
 }
 
